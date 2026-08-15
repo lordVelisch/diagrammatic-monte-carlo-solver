4bef9909fee6409b9bee9f2e5d5e4f0dafe290dc
Valentin Mayer <valentinmayer1@gmail.com>
2026-08-15 19:33:13 +0200

    extracting observable calculation outside of sampling loop and calculate integral results

--- src/general_DMC_order_0.cpp
+++ src/general_DMC_order_0.cpp
@@ -7,6 +7,8 @@
 #include <vector>
 #include <numeric>
 
+const double Z = 1 - std::exp(-5); // normalization factor
+
 // better to store struct or something else?
 struct D {
     double alpha, tau;
@@ -16,17 +18,10 @@ double target_pdf(D diag) {
     return std::exp(-diag.tau * diag.alpha);
 }
 
-const double Z = 1 - std::exp(-5); // normalization factor
-
-
-// reference okay here? Idea was maybe I can easier pass the diag_new into multiple change_x functions
-// but maybe it is just better to create inside the function and return if there is at maximum only 1 mutation done
 void change_tau(D &diag, const double delta, std::mt19937 &rng) {
     std::uniform_real_distribution<double> random_uniform(0, 1);
 
     double tau_proposed = diag.tau + delta * (random_uniform(rng) - 0.5);
-    // this way it will still calculated and get 1 as result.
-    //find better solution long term
 
     diag.tau = (tau_proposed <= 5 && tau_proposed >= 0) ? tau_proposed : diag.tau;
 }
@@ -37,47 +32,31 @@ void change_alpha(D &diag, std::mt19937 &rng) {
     diag.alpha = random_bernoulli(rng) ? 0.5 : 1;
 }
 
-// A is the observable to calculate
-template<typename Func>
-double sample_distribution(std::mt19937 &rng, const double delta, Func observable,
-                                        int N = 1'000'000, std::ofstream* histogram_output = nullptr) {
+std::vector<D> sample_distribution(std::mt19937 &rng, const double delta, int N = 1'000'000) {
 
-    // file writing for plotting of diagrams, this is a bit bothersome to have inside
-    // and if statements with write_to_file everytime also seems a bit clunky. better way available or fine?
-    if (histogram_output)
-        *histogram_output << "alpha,tau\n";
-
-    std::uniform_real_distribution<double> random_uniform(0, 1);
+    std::uniform_real_distribution<> random_uniform(0, 1);
     std::bernoulli_distribution random_bernoulli(0.5);
 
-    D D_initial = {.alpha = 1, .tau = random_uniform(rng) * 5};
+    D d_initial = {.alpha = 1, .tau = random_uniform(rng) * 5};
 
-    double A_sum = 0;
-
-    D d_curr = D_initial;
-
+    std::vector<D> D_values = {d_initial};
+    D_values.reserve(N);
     for (int i = 1; i < N; i++) {
-
-        D diag_new = d_curr;
+        D d_curr = D_values.back();
+        D d_new = d_curr;
 
         // 50/50 which change will be done
-        random_bernoulli(rng) ? change_tau(diag_new, delta, rng) : change_alpha(diag_new, rng);
-
-        if (histogram_output)
-            *histogram_output << d_curr.alpha << "," << d_curr.tau << "\n";
-
-        double p_acc = std::min(1., target_pdf(diag_new) / target_pdf(d_curr));
+        random_bernoulli(rng) ? change_tau(d_new, delta, rng) : change_alpha(d_new, rng);
+        double p_acc = std::min(1., target_pdf(d_new) / target_pdf(d_curr));
 
         if (random_uniform(rng) < p_acc) {
-            A_sum+=observable(diag_new);
-            d_curr = diag_new;
+            D_values.push_back(d_new);
         } else {
-            A_sum+=observable(d_curr); // this I could theoretically store in temp variable
+            D_values.push_back(d_curr);
         }
     }
-    return A_sum/N;
+    return D_values;
 }
-
 
 const double I1_exact = 1. - 6. * std::exp(-5);
 const double I2_exact = 2. - 37. * std::exp(-5);
@@ -86,13 +65,15 @@ int main() {
     std::random_device rd;
     std::mt19937 rng(rd());
 
-    // just for histogram
+    std::vector<D> diags = sample_distribution(rng, 10, 1'000'000);
+
     std::ofstream histogram_output(std::string(PROJECT_ROOT) + "/data/task5/histogram.csv");
+    histogram_output << "alpha,tau\n";
 
-    sample_distribution(rng, 10, [](D diag) {return diag.tau;}, 1'000'000, &histogram_output);
-    sample_distribution(rng, 10, [](D diag) { return diag.alpha; }, 1'000'000);
+    // I could do another blocking analysis here (or insert the code to check the actual errors)
 
-
-
+    for (const auto [alpha, tau] : diags) {
+        histogram_output << alpha << "," << tau << "\n";
+    }
     return 0;
 }
